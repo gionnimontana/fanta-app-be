21dc25eb783f204b8fa4c874d28ea88434e4524c
gmontanari <gmontanari@pdxeng.ch>
2022-08-14 13:21:23 +0300

    add hasura db connection for scrape query saving

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const puppeteer = require('puppeteer');
-
-const URL = 'https://www.fantacalcio.it/voti-fantacalcio-serie-a';
+require('dotenv').config({path: __dirname + '/.env'})
+const puppeteer = require('puppeteer')
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const statsScraper = async () => {
+    const URL = 'https://www.fantacalcio.it/voti-fantacalcio-serie-a';
     console.log('Opening the browser...')
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
@@ -40,9 +41,63 @@ const statsScraper = async () => {
     await browser.close()
 
     console.log('Job done!')
-    console.log('gamesWeek', gamesWeek)
-    console.log('TEST first team stats:', stats[0].players)
-    return stats
+
+    const output = stats.reduce((acc, team) => {
+        return [...acc, ...team.players.map(player => ({
+            giornata: gamesWeek,
+            squadra: team.name,
+            giocatore: player.name,
+            voto: Number(player.vote.replace(',', '.')),
+            fantavoto: Number(player.fvote.replace(',', '.')),
+            id: `${gamesWeek}-${team.name}-${player.name}`,
+        }))]
+    }, [])
+
+    return output
 };
 
-statsScraper()
+async function fetchGraphQL(operationsDoc, operationName, variables) {
+	const result = await fetch(process.env.GRAPHQL_URL, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'x-hasura-admin-secret': process.env.GRAPHQL_TOKEN
+		},
+		body: JSON.stringify({
+			query: operationsDoc,
+			variables: variables,
+			operationName: operationName
+		})
+	})
+
+	return await result.json()
+}
+
+async function writeStats(stats) {
+	const json = JSON.stringify(stats)
+	const unquoted = json.replace(/"([^"]+)":/g, '$1:')
+
+	const mutationString = `
+        mutation insert_fanta_stats {
+            insert_fanta_stats(
+                objects: ${unquoted},
+                on_conflict: {
+                    constraint: fanta_stats_pkey,
+                    update_columns: [voto, fantavoto]
+                }
+            ) {
+                affected_rows
+            }
+        }
+    `
+
+    return await fetchGraphQL(mutationString, 'insert_fanta_stats', {})
+}
+
+const main = async () => {
+    const stats = await statsScraper()
+    const result = await writeStats(stats)
+    console.log(result) 
+}
+
+main()
